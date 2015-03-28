4334b1ba1ab25c2114cd345fa839cc0a588d696d
Alex Horan <a_horan3@knights.ucf.edu>
2015-03-28 17:21:18 -0400

    initial work to google maps setup

---
+++ JsonDataStructures.js
@@ -0,0 +1,32 @@
+/*FILE FOR REFERENCE ONLY */
+
+/** GET FACILITIES ********
+/rest/channel/17652/facilities?q=list&skip=0&take=100
+@ Returns array of cities
+* Ex: return of a facility
+*/
+{
+    "ID": 10640,
+    "Latitude": 42.2197227,
+    "Longitude": -83.02769,
+    "Name": " (DEV)",
+    "Address": {
+        "City": "Windsor",
+        "Country": "CA",
+        "Line1": "1300 Disputed Rd.",
+        "Line2": "",
+        "PostalCode": "N9A6Z6",
+        "StateProvinceCode": "ON"
+    },
+    "CurrencyCode": "CAD",
+    "Description": "",
+    "EmailAddress": null,
+    "Information": "Price includes golf and cart. Tax not included. Taxes have been estimated. Final taxes will be calculated at the golf course, based on local tax rates for green fees and golf cart. Not valid with any other offers or promotions.",
+    "IsActive": true,
+    "PhoneNumber": "(000) 000-0000",
+    "Tags": [],
+    "TeeTimePolicy": "You may cancel your tee time up to 24 hours in advance without penalty ONLY by calling GolfNow Customer Service at 800-767-3574. If you DO NOT call GolfNow Customer Service to cancel your tee time your credit card WILL be charged. ANY tee time booked within 24 hours is guaranteed for the full amount at the time of processing and cannot be cancelled.",
+    "ThumbnailImagePath": "www.golfnow.com/coursedirectory/img/on/windsor/seven-lakes-golf-club/general.jpgx",
+    "TimeZoneOffset": -4,
+    "WebsiteAddress": "http://www.sevenlakesgolf.com"
+}
